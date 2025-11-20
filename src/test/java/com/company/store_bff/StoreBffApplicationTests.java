f494d339ceeb0c7ea2d8b9927a4f9612a2448dc7
Alejandro Morales <amoralesnaranjo@gmail.com>
2025-11-20 07:25:29 +0100

    Initial commit

---
+++ src/test/java/com/company/store_bff/StoreBffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.store_bff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
